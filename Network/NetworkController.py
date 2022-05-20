aff2d152e4b1d47d8b69554c406690b2f545c23a
Jelmur <Jelmervdermeulen@gmail.com>
2022-05-20 11:41:14 +0200

    Moved config() from NetworkController.py to it's own file.

--- Network/NetworkController.py
+++ Network/NetworkController.py
@@ -1,56 +1,14 @@
-import os
 import platform
+from ConfigReader import config
 import socket
 import json
-from configparser import ConfigParser
 from Logger.ConsoleLogger import ConsoleLogger
 from Logger.FileLogger import FileLogger
-
-
-def config(section='PC'):
-    """
-    Returns an object containing the properties and values needed to connect to the corresponding database
-
-    :param section: Is either "staging" or "final". Defines the section of the config file in database.ini.
-    If not defined defaults to "staging"
-    """
-    match platform.system():
-        case "Windows":
-            logger = ConsoleLogger()
-        case "Linux":
-            logger = FileLogger()
-        case _:
-            logger = FileLogger()
-            logger.log("System not recognized")
-
-    while section not in ["PC", "Pi"]:
-        logger.log(f'\033[1;31mWrong dbType: {section}\nIt should be either "staging" or "final"\033[1;37m')
-        return
-
-    parser = ConfigParser()
-    cwd = os.getcwd()
-    if cwd[-1] == 'o':
-        parser.read('../Config.ini')
-    else:
-        parser.read('Config.ini')
-
-    values = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            values[param[0]] = param[1]
-    else:
-        logger.log(str(Exception("Section {0} not found in database.ini".format(section))))
-    return values
 
 
 class NetworkController:
 
     def __init__(self):
-        self.ip_address = NetworkController.params['ip_address']
-        self.port = int(NetworkController.params['port'])
-        self.buffer_size = 1024
-        self.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         match platform.system():
             case "Windows":
                 self.params = config()
@@ -61,6 +19,10 @@ class NetworkController:
             case _:
                 self.logger = FileLogger()
                 self.logger.log("System not recognized")
+        self.ip_address = self.params['ip_address']
+        self.port = int(self.params['port'])
+        self.buffer_size = 1024
+        self.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
     def setup_server(self):
         """
@@ -70,7 +32,6 @@ class NetworkController:
         self.udp_server_socket.bind((self.ip_address, self.port))
         self.logger.log("Server online")
         self.__start_listening()
-
 
     def __start_listening(self):
         """
@@ -111,16 +72,3 @@ class NetworkController:
 
     def send_message(self, bytes_to_send, address):
         self.udp_server_socket.sendto(bytes_to_send, address)
-
-
-
-
-
-
-
-
-
-
-
-
-
