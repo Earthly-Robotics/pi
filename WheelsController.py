f7617ad06bd2dd45cd7f0d70c5538f73c164c56f
Wim <w.j.stienstra@home.nl>
2022-06-03 11:38:34 +0200

    Removed unneccesary stuff

--- WheelsController.py
+++ WheelsController.py
@@ -1,50 +1,37 @@
 from WheelMotor import *
-from time import sleep
-import gpiozero as GPIO
 
 
 class WheelsController:
-    # def __init__(self):
-
     def move_logic(self, cmd):
 
-        motor_left = WheelMotor( 19, 16, 13)
-        # 13 PWM
+        motor_left = WheelMotor(19, 16, 13)
         motor_right = WheelMotor(9, 10, 14)
-
 
         # forwards
         if cmd.joystick_position_y == 1 and cmd.joystick_position_x < 1010 and cmd.joystick_position_x > 100:
             motor_left.move(0.5)
             motor_right.move(0.5)
-
-            sleep(5)
             print("forwards")
 
         # backwards
         if cmd.joystick_position_y > 1010 and cmd.joystick_position_x < 1010 and cmd.joystick_position_x > 100:
             motor_left.move(0.5)
             motor_right.move(0.25)
-
-            sleep(5)
             print("backwards")
 
         # left
         if cmd.joystick_position_x == 1 and cmd.joystick_position_y < 1010 and cmd.joystick_position_y > 100:
             motor_left.move(0.5)
             motor_right.move(0.5)
-
-            sleep(5)
             print("left")
 
         # right
         if cmd.joystick_position_x > 1010 and cmd.joystick_position_y < 1010 and cmd.joystick_position_y > 100:
             motor_left.move(0.5)
             motor_right.move(0.5)
-            sleep(5)
             print("right")
 
-        #top-right
+        # top-right
         if cmd.joystick_position_x > 1010 and cmd.joystick_position_y == 1:
             print("top-right")
             # motor_left.move(0.2)
@@ -66,29 +53,22 @@ class WheelsController:
             motor_left.move(0.5)
             motor_right.move(0.25)
 
-        #top-left
+        # top-left
         if cmd.joystick_position_x == 1 and cmd.joystick_position_y == 1:
             motor_left.move(0.25)
             motor_right.move(0.5)
-
-            sleep(5)
             print("top-left")
 
-        #bottom-right
+        # bottom-right
         if cmd.joystick_position_x > 1010 and cmd.joystick_position_y > 1010:
             motor_left.move(0.25)
             motor_right.move(0.5)
-
-            sleep(5)
             print("bottom-right")
 
-        #bottom-left
+        # bottom-left
         if cmd.joystick_position_x == 1 and cmd.joystick_position_y > 1010:
             motor_left.move(0.5)
             motor_right.move(0.25)
-
-            sleep(5)
             print("bottom-left")
 
         # https://sensorkit.joy-it.net/en/sensors/ky-023
-
