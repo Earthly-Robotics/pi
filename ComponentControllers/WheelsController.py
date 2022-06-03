0455f7612d86e938ae201bfde400ecb6e1803201
Jelmur <Jelmervdermeulen@gmail.com>
2022-06-03 13:31:06 +0200

    Changed hardcoded values to variable

--- ComponentControllers/WheelsController.py
+++ ComponentControllers/WheelsController.py
@@ -7,36 +7,39 @@ class WheelsController:
 
         motor_left = WheelMotor(19, 16, 13)
         motor_right = WheelMotor(9, 10, 14)
+        max_mid = 500
+        min_mid = 400
 
-        if 400 < x < 500 and 400 < y < 500:
+
+        if min_mid < x < max_mid and min_mid < y < max_mid:
             motor_left.move(0)
             motor_right.move(0)
         # forwards
-        if y > 500 and x < 1010 and x > 100:
+        if y > max_mid and min_mid < x < max_mid:
             motor_left.move(0.5)
             motor_right.move(0.5)
             print("forwards")
 
         # backwards
-        if y < 400 and x < 1010 and x > 100:
+        if y < min_mid and min_mid < x < max_mid:
             motor_left.move(0.5)
             motor_right.move(0.25)
             print("backwards")
 
         # left
-        if x < 400 and y < 1010 and y > 100:
+        if x < min_mid and min_mid < y < max_mid:
             motor_left.move(0.5)
             motor_right.move(0.5)
             print("left")
 
         # right
-        if x > 500 and y < 1010 and y > 100:
+        if x > max_mid and min_mid < y < max_mid:
             motor_left.move(0.5)
             motor_right.move(0.5)
             print("right")
 
         # top-right
-        if x > 1010 and y > 1010:
+        if x > max_mid and y > max_mid:
             print("top-right")
             # motor_left.move(0.2)
             # motor_left.move(0.4)
@@ -58,19 +61,19 @@ class WheelsController:
             motor_right.move(0.25)
 
         # top-left
-        if x == 1 and y == 1010:
+        if x < min_mid and y > max_mid:
             motor_left.move(0.25)
             motor_right.move(0.5)
             print("top-left")
 
         # bottom-right
-        if x > 1010 and y == 1:
+        if x > max_mid and y < min_mid:
             motor_left.move(0.25)
             motor_right.move(0.5)
             print("bottom-right")
 
         # bottom-left
-        if x == 1 and y == 1:
+        if x < min_mid and y < min_mid:
             motor_left.move(0.5)
             motor_right.move(0.25)
             print("bottom-left")
