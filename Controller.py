1498ca630a4e3ad9a02843566dbdf1cff0723759
Wim <w.j.stienstra@home.nl>
2022-06-08 16:54:37 +0200

    Motors with PWM working

--- Controller.py
+++ Controller.py
@@ -5,9 +5,18 @@ from Components.CameraFeed import CameraFeed
 
 
 def main():
+    # vision_controller = VisionController()
+    # camera_feed = CameraFeed(vision_controller.cam)
     wheels_controller = WheelsController()
+    # server = NetworkController(wheels_controller, vision_controller, camera_feed)
     server = NetworkController(wheels_controller)
     server.setup_server()
+
+    # vision_controller = VisionController()
+    #
+    # while True:
+    #     vision_controller.get_camera_feed()
+    #     cv.waitKey(1)
 
 
 main()
