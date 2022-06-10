34b3e8e3c41ce992aceeeb135835fbc56420c111
Jelmur <Jelmervdermeulen@gmail.com>
2022-06-10 20:53:30 +0200

    Moving the wheels is now multi-threaded. Still needs to be tested.

--- Controller.py
+++ Controller.py
@@ -3,20 +3,21 @@ import time
 from Network.NetworkController import *
 import threading
 # from ComponentControllers.VisionController import VisionController
-# from ComponentControllers.WheelsController import WheelsController
+from ComponentControllers.WheelsController import WheelsController
 
 
 def main():
     # vision_controller = VisionController()
     # camera_feed = CameraFeed(vision_controller.cam)
-    # wheels_controller = WheelsController()
+    wheels_controller = WheelsController()
     # server = NetworkController(wheels_controller, vision_controller, camera_feed)
-    # server = NetworkController(wheels_controller)
-    server = NetworkController()
-    thread = threading.Thread(target=server.setup_server, daemon=True)
-    thread.start()
-    time.sleep(100)
-    print("done with main")
+    server = NetworkController(wheels_controller)
+    server.setup_server()
+    # server = NetworkController()
+    # thread = threading.Thread(target=server.setup_server, daemon=True)
+    # thread.start()
+    # time.sleep(100)
+    # print("done with main")
 
     # vision_controller = VisionController()
     #
