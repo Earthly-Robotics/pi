7e2ea207a56f477bbc445b91cf8618a8c0acfc82
Jelmur <Jelmervdermeulen@gmail.com>
2022-06-14 14:38:40 +0200

    Joystick input now fully converted to motor speed.

--- ComponentControllers/WheelsController.py
+++ ComponentControllers/WheelsController.py
@@ -12,6 +12,8 @@ class WheelsController:
         self.network_controller = None
         self.motor_left = WheelMotor(19, 26, 13)
         self.motor_right = WheelMotor(16, 20, 12)
+        self.per_x = 0
+        self.per_y = 0
         
     def move_wheels(self, x, y):
         """
@@ -20,70 +22,103 @@ class WheelsController:
         :param y: joystick y position
         :return:
         """
+        max_mid = 0  # Deadzone positive x and y
+        min_mid = -400  # Deadzone negative x and y
+        Top = 2047
+        Bottom = 2047
 
-        max_mid = 500  # Deadzone positive x and y
-        min_mid = 400  # Deadzone negative x and y
-        power_left = 0
-        power_right = 0
+        # if min_mid < x < max_mid and min_mid < y < max_mid:
+        #     power_left_motor = 0
+        #     power_right_motor = 0
+        #     self.motor_left.move(power_left_motor)
+        #     self.motor_right.move(power_right_motor)
+        # else:
+        #     x = math.floor((x / 1750) * 100) * -1
+        #     y = math.floor((y / 1750) * 100)
+        #
+        #     right_plus_left = (100-abs(x)) * (y/100) + y
+        #     right_minus_left = (100-abs(y)) * (x/100) + x
+        #     power_left_motor = (right_plus_left-right_minus_left)/2
+        #     power_right_motor = (right_plus_left+right_minus_left)/2
+        #     print("Left power: ", power_left_motor)
+        #     # print("Right power: ", power_right_motor)
+        #     if power_left_motor > 100:
+        #         power_left_motor = 100
+        #     if power_left_motor < -100:
+        #         power_left_motor = -100
+        #     if power_right_motor > 100:
+        #         power_right_motor = 100
+        #     if power_right_motor < -100:
+        #         power_right_motor = -100
+        #     self.motor_left.move(math.floor(power_left_motor))
+        #     self.motor_right.move(math.floor(power_right_motor))
+
+        # power_left = 0
+        # power_right = 0
+
         if min_mid < x < max_mid and min_mid < y < max_mid:
-            power_left = 0
-            power_right = 0
-
+            per_x = 0
+            per_y = 0
+            self.motor_left.move(per_x)
+            self.motor_right.move(per_y)
+            print("Stop")
         # forwards
-        if y > max_mid > x > min_mid:
-            percent = math.floor(((y - max_mid) / 521) * 100)
-            power_left = percent
-            power_right = percent
+        elif y > max_mid > x > min_mid:
+            per_x = 0
+            per_y = math.floor(((y - max_mid) / (Top - max_mid)) * 100)
+            self.move(per_x, per_y)
             print("forwards")
 
         # backwards
-        if y < min_mid < x < max_mid:
-            percent = math.floor(((y - min_mid) / min_mid) * 100)
-            power_left = percent
-            power_right = percent
+        elif y < min_mid < x < max_mid:
+            per_x = 0
+            per_y = math.floor((y - min_mid) / (Bottom + min_mid) * 100)
+            self.move(per_x, per_y)
             print("backwards")
-
         # left
-        if x < min_mid < y < max_mid:
-            percent = math.floor(((x - min_mid) / min_mid) * 100) # negative number
-            power_left = percent # negative
-            power_right = -1 * percent # positive
+        elif x < min_mid < y < max_mid:
+            per_x = math.floor((x - min_mid) / (Bottom + min_mid) * 100) * -1
+            per_y = 0
+            self.move(per_x, per_y)
             print("left")
-
         # right
-        if x > max_mid > y > min_mid:
-            percent = math.floor(((x - max_mid) / 521) * 100) # positive number
-            power_left = percent # positive
-            power_right = -1 * percent # negative
+        elif x > max_mid > y > min_mid:
+            per_x = math.floor((x - max_mid) / (Top - max_mid) * 100) * -1
+            per_y = 0
+            self.move(per_x, per_y)
             print("right")
+        # top-right
+        elif x > max_mid and y > max_mid:
+            per_x = math.floor((x - max_mid) / (Top - max_mid) * 100) * -1
+            per_y = math.floor((y - max_mid) / (Top - max_mid) * 100)
+            self.move(per_x, per_y)
+            print("top-right")
+        # top-left
+        elif x < min_mid and y > max_mid:
+            per_x = math.floor((x - min_mid) / (Bottom + min_mid) * 100) * -1
+            per_y = math.floor((y - max_mid) / (Top - max_mid) * 100)
+            self.move(per_x, per_y)
+            print("top-left")
+        # bottom-right
+        elif x > max_mid and y < min_mid:
+            per_x = math.floor((x - max_mid) / (Top - max_mid) * 100) * -1
+            per_y = math.floor((y - min_mid) / (Bottom + min_mid) * 100)
+            self.move(per_x, per_y)
+            print("bottom-right")
+        # bottom-left
+        elif x < min_mid and y < min_mid:
+            per_x = math.floor((x - min_mid) / (Bottom + min_mid) * 100) * -1
+            per_y = math.floor((y - min_mid) / (Bottom + min_mid) * 100)
+            self.move(per_x, per_y)
+            print("bottom-left")
 
-        # # top-right
-        # elif x > max_mid and y > max_mid:
-        #     print("top-right")
-        #     percent = math.floor(((x - max_mid + y - max_mid) / 512) * 100)
-        #     print(percent)
-        #     self.power_left = 100
-        #     self.power_right = 50
-        #
-        # # top-left
-        # elif x < min_mid and y > max_mid:
-        #     self.power_left = 100
-        #     self.power_right = 50
-        #     print("top-left")
-        #
-        # # bottom-right
-        # elif x > max_mid and y < min_mid:
-        #     self.power_left = 100
-        #     self.power_right = 50
-        #     print("bottom-right")
-        #
-        # # bottom-left
-        # elif x < min_mid and y < min_mid:
-        #     self.power_left = 100
-        #     self.power_right = 50
-        #     print("bottom-left")
+    def move(self, per_x, per_y):
+        right_plus_left = ((100 - abs(per_x)) * (per_y / 100) + per_y)  # 200
+        right_minus_left = ((100 - abs(per_y)) * (per_x / 100) + per_x)  # 0
+        power_left_motor = (right_plus_left - right_minus_left) / 2
+        power_right_motor = (right_plus_left + right_minus_left) / 2
+        self.motor_left.move(math.floor(power_left_motor))
+        self.motor_right.move(math.floor(power_right_motor))
 
-        self.motor_left.move(power_left)
-        self.motor_right.move(power_right)
 
 # https://sensorkit.joy-it.net/en/sensors/ky-023
